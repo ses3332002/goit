0e6c9cc749a030ff83d87c1ebee52bb1e2425efc
Sergey Gavrilov <ses3332002@gmail.com>
2016-09-26 13:42:29 +0300

    fourth commit

--- js/homework11-12/12/js/script12.js
+++ js/homework11-12/12/js/script12.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-  var newItem = $("#tmplt").html();
+  var newItem = $('#tmplt').html();
   var action_stars = [
     {
-      name: "Брюс",
-      lastName: "Ли",
-      state: "Гонконг",
-      birth: "1940",
-      death: "1973"
+      name: 'Брюс',
+      lastName: 'Ли',
+      state: 'Гонконг',
+      birth: '1940',
+      death: '1973'
     },
     {
-      name: "Саммо",
-      lastName: "Хун",
-      state: "Гонконг",
-      birth: "1952",
-      death: "-"
+      name: 'Саммо',
+      lastName: 'Хун',
+      state: 'Гонконг',
+      birth: '1952',
+      death: '-'
     },
     {
-      name: "Джеки",
-      lastName: "Чан",
-      state: "Гонконг",
-      birth: "1954",
-      death: "-"
+      name: 'Джеки',
+      lastName: 'Чан',
+      state: 'Гонконг',
+      birth: '1954',
+      death: '-'
     },
     {
-      name: "Джет",
-      lastName: "Ли",
-      state: "Китай",
-      birth: "1963",
-      death: "-"
+      name: 'Джет',
+      lastName: 'Ли',
+      state: 'Китай',
+      birth: '1963',
+      death: '-'
     }
   ];
   var content = tmpl(newItem, {data: action_stars});
   
-  $(".wrapper").append(content);
+  $('.wrapper').append(content);
   
 });
