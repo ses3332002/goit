c71eed7651aaa9657cd195ba8a22094f84730207
Sergey Gavrilov <ses3332002@gmail.com>
2016-10-04 11:32:19 +0300

    second commit

--- js/homework13-14/js/script13.js
+++ js/homework13-14/js/script13.js
@@ -1,7 +1,7 @@
-'use strict;'
+'use strict';
 
 $(document).ready(function(){
-  
+
   //формирование вопросов и ответов для теста
 
   var questionList = [];
@@ -24,45 +24,37 @@ $(document).ready(function(){
     questionAnswer: '0101'
   };
 
+  questionList[3] = {
+    questionText: 'Сколько будет 5+5?',
+    questionOptions: ['10','11','12'],
+    questionAnswer: '100'
+  };
+
   //записываем вопросы в локальное хранилище
 
-  localStorage.setItem('q0', JSON.stringify(questionList[0]));
-  localStorage.setItem('q1', JSON.stringify(questionList[1]));
-  localStorage.setItem('q2', JSON.stringify(questionList[2]));
+  localStorage.setItem('questions', JSON.stringify(questionList));
 
   //создание страницы и размещение вопросов теста
 
   var onPageQuestions = [];
-  onPageQuestions[0] = JSON.parse(localStorage.getItem('q0'));
-  onPageQuestions[1] = JSON.parse(localStorage.getItem('q1'));
-  onPageQuestions[2] = JSON.parse(localStorage.getItem('q2'));
+  onPageQuestions = JSON.parse(localStorage.getItem('questions'));
 
   var newItem = $('#tmplt').html();
   var content = tmpl(newItem, {data: onPageQuestions});
   $('.formContent').append(content);
 
   //вызов отчета о результатах
-  
+
   $('.submitButton').mousedown(function(){
     var res = calculateResults();
     $('.resultsBack').show();
     $('.message').text('Вы правильно ответили на ' + res + ' вопрос(a/ов) из ' + onPageQuestions.length);
     $('.resultsMessage').show();
-  });  
-  
-  //установка атрибутов чекбоксов
-  
-  $('.option input').click(function(){
-    if(!$(this).attr('checked')) {
-      $(this).attr('checked', true)
-    } else {
-      $(this).attr('checked', false)
-    }
   });
-  
+
   //расчет результатов теста
-  
-  calculateResults = function() {
+
+  var calculateResults = function() {
     var rightAnswers = 0;
     for (var q=0; q < onPageQuestions.length; q++) {
       if (checkQuestion(q)) {
@@ -71,16 +63,17 @@ $(document).ready(function(){
     }
     return rightAnswers;
   };
-  
+
   //расчет результатов по конкретному вопросу
-  
-  checkQuestion = function(numberOfQuestion) {
+
+  var checkQuestion = function(numberOfQuestion) {
     var rightMatrix = onPageQuestions[numberOfQuestion].questionAnswer;
     var currentMatrix = '';
     var optionId;
     for (var o=0; o<onPageQuestions[numberOfQuestion].questionOptions.length; o++) {
       optionId = (numberOfQuestion+1)*10 + (o+1);
-      if ($('#' + optionId).attr('checked')) {
+      if ($('#' + optionId).prop('checked')) {
+      // if ($('#' + optionId).attr('checked')) {
         currentMatrix = currentMatrix + '1';
       } else {
         currentMatrix = currentMatrix + '0';
@@ -92,6 +85,13 @@ $(document).ready(function(){
       return false;
     }
   }
-  
-});
 
+  //закрытие окна с результатами
+
+  $('.messageButton').click(function(){
+    $('.resultsMessage').hide();
+    $('.resultsBack').hide();
+    $('input:checked').prop('checked',false);
+  });
+
+});
