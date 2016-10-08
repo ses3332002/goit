c42b6fd19b60b65ad9c5f49860835605f99f4a68
Sergey Gavrilov <ses3332002@gmail.com>
2016-10-08 20:42:26 +0300

    second commit

--- js/homework15-16/16/js/script16.js
+++ js/homework15-16/16/js/script16.js
@@ -1,48 +1,48 @@
 'use strict';
 
-function Human() {
-  this.name = 'John';
+function Human(outName) {
+  this.name = outName;
   this.age = '25';
   this.sex = 'man';
   this.height = '175';
   this.weight = '75';
 };
 
-function Worker() {
-  this.workPlace = 'plant';
+function Worker(outName, outWorkPlace) {
+  Human.call(this,outName);
+  this.workPlace = outWorkPlace;
   this.salary = 1000;
   this.doWork = function () {
     alert(this.name + ': I am doing my job on ' + this.workPlace)
   };
 };
 
-function Student() {
-  this.studyPlace = 'institute';
+function Student(outName, outStudyPlace) {
+  Human.call(this,outName);
+  this.studyPlace = outStudyPlace;
   this.stipend = 100;
   this.watchTVShows = function () {
     alert(this.name + ': I am watching TV when not studying in the ' + this.studyPlace)
   };
 };
 
-Worker.prototype = new Human();
-Student.prototype = new Human();
+Worker.prototype = Object.create(Human.prototype);
+Worker.prototype.constructor = Worker;
+Student.prototype = Object.create(Human.prototype);
+Student.prototype.constructor = Student;
 
-var worker1 = new Worker();
-worker1.workPlace = 'factory';
+var worker1 = new Worker('John', 'factory');
 worker1.doWork();
-console.log(worker1);
+console.dir(worker1);
 
-var worker2 = new Worker();
-worker2.name = 'Nick';
+var worker2 = new Worker('Nick', 'plant');
 worker2.doWork();
-console.log(worker2);
+console.dir(worker2);
 
-var student1 = new Student();
-student1.name = 'Stephen';
+var student1 = new Student('Stephen', 'institute');
 student1.watchTVShows();
-console.log(student1);
+console.dir(student1);
 
-var student2 = new Student();
-student2.studyPlace = 'high school';
+var student2 = new Student('Tom', 'high school');
 student2.watchTVShows();
-console.log(student2);
+console.dir(student2);
