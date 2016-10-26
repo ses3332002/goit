ccf5a292c08066095db96f77b368b92374c57cbd
Sergey Gavrilov <ses3332002@gmail.com>
2016-10-26 11:35:38 +0300

    third commit

---
+++ js/homework23-24/requirejs/js/view.js
@@ -0,0 +1,31 @@
+define (
+  'view',
+  [],
+  function () {
+    return {
+      view: function(model) {
+        var self = this;
+
+        self.elements = {
+          input: $('.service__input'),
+          addBtn: $('.service__add'),
+          taskContainer: $('.tasks')
+        };
+
+        self.initPage = function() {
+          self.elements.taskContainer.html('');
+          self.renderList(model.data);
+          self.elements.input.val('');
+        };
+
+        self.renderList = function(data) {
+          for (var i = 0; i < data.length; i++) {
+            self.elements.taskContainer.append('<div class="tasks__task"><div class="tasks__txt">'+data[i]+'</div><div class="tasks__edt">&#9998;</div><div class="tasks__rmv">-</div></div>');
+          };
+        };
+
+        self.initPage();
+      }
+    };
+  }
+);
