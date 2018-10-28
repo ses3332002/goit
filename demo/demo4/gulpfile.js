9b96031923789563062473a1585e04073cecc6b9
Sergey Gavrilov <ses3332002@gmail.com>
2018-10-28 14:59:42 +0200

    added favicons generation in gulp for demo4

--- demo/demo4/gulpfile.js
+++ demo/demo4/gulpfile.js
@@ -15,7 +15,97 @@ var gcmq = require('gulp-group-css-media-queries');
 var rigger = require('gulp-rigger');
 var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
+var realFavicon = require ('gulp-real-favicon');
+var fs = require('fs');
 
+var FAVICON_DATA_FILE = 'src/icons/faviconData.json';
+
+gulp.task('generate-favicon', function(done) {
+	realFavicon.generateFavicon({
+		masterPicture: 'src/icons/logo_templ.png',
+		dest: './',
+		iconsPath: '/',
+		design: {
+			ios: {
+				pictureAspect: 'noChange',
+				assets: {
+					ios6AndPriorIcons: false,
+					ios7AndLaterIcons: false,
+					precomposedIcons: false,
+					declareOnlyDefaultIcon: true
+				}
+			},
+			desktopBrowser: {},
+			windows: {
+				pictureAspect: 'noChange',
+				backgroundColor: '#da532c',
+				onConflict: 'override',
+				assets: {
+					windows80Ie10Tile: false,
+					windows10Ie11EdgeTiles: {
+						small: false,
+						medium: true,
+						big: false,
+						rectangle: false
+					}
+				}
+			},
+			androidChrome: {
+				pictureAspect: 'noChange',
+				themeColor: '#ffffff',
+				manifest: {
+					display: 'standalone',
+					orientation: 'notSet',
+					onConflict: 'override',
+					declared: true
+				},
+				assets: {
+					legacyIcon: false,
+					lowResolutionIcons: false
+				}
+			},
+			safariPinnedTab: {
+				pictureAspect: 'blackAndWhite',
+				threshold: 50,
+				themeColor: '#5bbad5'
+			}
+		},
+		settings: {
+			scalingAlgorithm: 'Mitchell',
+			errorOnImageTooSmall: false,
+			readmeFile: false,
+			htmlCodeFile: false,
+			usePathAsIs: false
+		},
+		markupFile: FAVICON_DATA_FILE
+	}, function() {
+		done();
+	});
+});
+
+gulp.task('inject-favicon-markups', function() {
+	return gulp.src([ './src/icons/head.html' ])
+		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+		.pipe(gulp.dest('./src/html/templates'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+	realFavicon.checkForUpdates(currentVersion, function(err) {
+		if (err) {
+			throw err;
+		}
+	});
+});
+
+gulp.task('icons', function() {
+  runSequence('generate-favicon',
+  'inject-favicon-markups');
+});
 
 gulp.task('webserver', function() {
   gulp.src('./')
@@ -30,7 +120,7 @@ gulp.task('webserver', function() {
 gulp.task('imagemin', function() {
   gulp.src('./src/img/*.*')
     .pipe(imagemin({
-      optimizationLevel: 3,
+      optimizationLevel: 7,
       progressive: true,
       interlaced: true
     }))
