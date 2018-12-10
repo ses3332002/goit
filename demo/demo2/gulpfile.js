00b90464a7e33919d033c70bb705625d03ea0587
Sergey Gavrilov <ses3332002@gmail.com>
2018-12-10 23:43:59 +0200

    adapted to retina 2x. adaptive images.

--- demo/demo2/gulpfile.js
+++ demo/demo2/gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('sass', function () {
   gulp.src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 4 versions'],
+      browsers: ['last 3 versions'],
       cascade: false
     }))
     .pipe(gulp.dest('./src/css'));
