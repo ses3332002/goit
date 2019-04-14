2be4041bf92e65ea23d4e0a08a3954d46ad802ec
Sergey Gavrilov <ses3332002@gmail.com>
2019-04-14 17:24:35 +0300

    minor fixes

--- demo/demo3/gulpfile.js
+++ demo/demo3/gulpfile.js
@@ -76,7 +76,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
   gulp.watch('./src/scss/*.scss', ['sass', 'prepare_css']);
   gulp.watch('./src/js/*.js', ['prepare_js']);
-  gulp.watch('./src/html/*.html', ['prepare_html']);
+  gulp.watch('./src/html/**/*.html', ['prepare_html']);
 });
 
 gulp.task('prepare_html', function () {
