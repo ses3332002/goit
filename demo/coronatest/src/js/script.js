c97951fb3c097f410242d6e98b203f5a6a95c079
Sergey Gavrilov <ses3332002@gmail.com>
2020-03-28 14:16:47 +0200

    minor fixes

--- demo/coronatest/src/js/script.js
+++ demo/coronatest/src/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
   function startTest() {
     $('.info').slideUp("slow", function () {
-      $('.test_form').slideDown( "slow" );
+      $('.test_form').slideDown("slow");
       testBtn.textContent = "Отправить тест на проверку";
       testBtn.removeEventListener("click", startTest);
       testBtn.addEventListener("click", showResults);
@@ -15,10 +15,11 @@ $(document).ready(function() {
 
   function showResults() {
     $('.results_back').show();
-    $('.results_message').show();
+    $('.results_message').show("slow");
   };
 
   $('.message_button').click(function() {
-    document.location.reload(true);
+    $('input:checked').prop('checked',false);
+    document.location.reload(false);
   });
 });
